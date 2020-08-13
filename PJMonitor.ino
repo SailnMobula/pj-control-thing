ca155afedaaae03f39541e096d8fcf8b99845ce1
Julian Kramer <Julian.Kramer@EXXETA.com>
2020-08-13 11:16:34 +0200

    Refactor and remove unused code

--- PJMonitor.ino
+++ PJMonitor.ino
@@ -1,34 +1,62 @@
 #include "battery_info.h"
-// #include "ibs_service.h"
 #include "led_service.h"
 #include "samd.h"
 #include "timed_service.h"
+
+#include <ArduinoBLE.h>
 
 #define LED_OUTPUT_1 2
 
 int ledState = LOW;
 int previousBatteryLevel = 0;
-
+int previousLedDutyCycle = 0;
 uint32_t ledServiceState = 1;
 
-// ibs_service ibsService;
-led_service ledService(200, 2, 2000);
-timed_service timedService((uint32_t)5000 * 1000);
+led_service ledService(200, 2, 1000);
+timed_service longTimedService((uint32_t)5000 * 1000);
+timed_service shortTimedService((uint32_t)1 * 1000);
+
+BLEService bleBatteryService("180F");
+BLEUnsignedCharCharacteristic batteryLevelCharacteristic("2A19",
+                                                         BLERead | BLENotify);
+
+BLEService bleLedService("98194a8e-a697-4b49-93f5-25ca2602013c");
+BLEUnsignedCharCharacteristic
+    leftLightDutyCycleCharacteristic("4d5c48e8-509c-4f61-a39d-ccb8f78dee34",
+                                     BLEWrite | BLENotify);
+// BLEUnsignedCharCharacteristic
+//     rightLightDutyCycleCharacteristic("901960d4-1f3b-4898-a6f9-afe0f9044114",
+//                                       BLEWrite | BLENotify);
+
+void bleInit() {
+  if (!BLE.begin()) {
+    Serial.println("Failed to initialize BLE!");
+    while (1)
+      ;
+  }
+
+  BLE.setDeviceName("PJMonitor");
+  BLE.setLocalName("BatteryMonitor");
+
+  BLE.setAdvertisedService(bleBatteryService);
+  bleBatteryService.addCharacteristic(batteryLevelCharacteristic);
+  BLE.addService(bleBatteryService);
+
+  BLE.setAdvertisedService(bleLedService);
+  bleLedService.addCharacteristic(leftLightDutyCycleCharacteristic);
+  // bleLedService.addCharacteristic(rightLightDutyCycleCharacteristic);
+  BLE.addService(bleLedService);
+
+  BLE.advertise();
+}
 
 void setup() {
   Serial.begin(9600);
-  // while (!Serial);
   Serial.println("Serial initalized");
 
   pinMode(LED_BUILTIN, OUTPUT);
-}
 
-void serializeData(char *buffer, int temperature, int humidity, float x,
-                   float y, float z) {
-  sprintf(buffer,
-          "{\"data\":{\"temperature\":%d,\"humidity\":%d,\"accelerometerX\":%."
-          "2f,\"accelerometerY\":%.2f,\"accelerometerZ\":%.2f}}\r\n",
-          temperature, humidity, x, y, z);
+  bleInit();
 }
 
 void blinkLED() {
@@ -37,53 +65,41 @@ void blinkLED() {
   } else {
     ledState = LOW;
   }
-
   digitalWrite(LED_BUILTIN, ledState);
 }
 
-void switchLedState() {
+void handleBLE(BLEDevice central, uint8_t currentLedDutyCycle) {
+  if (central) {
+    if (central.connected()) {
 
-  switch (ledServiceState) {
-  case 1:
-    ledService.turnOn();
-    ledServiceState = 2;
-    break;
-  case 2:
-    ledService.turnOnByDutyCycle(10);
-    ledServiceState = 3;
-    break;
-  case 3:
-    ledService.turnOnByDutyCycle(70);
-    ledServiceState = 4;
-    break;
-  case 4:
-    ledService.turnOff();
-    ledServiceState = 1;
-    break;
+      uint8_t batteryLevel = 82;
+      if (batteryLevel != previousBatteryLevel) {
+        previousBatteryLevel = batteryLevel;
+        batteryLevelCharacteristic.writeValue(batteryLevel);
+      }
 
-  default:
-    ledServiceState = 1;
-    break;
+      if (leftLightDutyCycleCharacteristic.written()) {
+        uint8_t ledDimmValue;
+        ledService.turnOnByDutyCycle(leftLightDutyCycleCharacteristic.value());
+      }
+    }
   }
 }
 
 void loop() {
-  char serializedString[256];
-  float accelerationValues[3] = {0.0, 0.0, 0.0};
-  int temperature;
-  int humidity;
   int batteryLevel;
+  uint8_t currentDutyCycle;
 
   ledService.controlLed();
 
-  if (timedService.isTimeSlotActive()) {
+  BLEDevice central = BLE.central();
+
+  if (shortTimedService.isTimeSlotActive()) {
+    currentDutyCycle = ledService.getCurrentDutyCycle();
+    handleBLE(central, currentDutyCycle);
+  }
+
+  if (longTimedService.isTimeSlotActive()) {
     blinkLED();
-    Serial.println("Heartbeat");
-    switchLedState();
-    // ibsService.read();
-    // ibsService.write();
-    // ibsService.bruteforceRead();
-    // delay(10);
-    // ibsService.write();
   }
 }
